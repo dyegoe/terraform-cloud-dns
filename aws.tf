3176e69d0c2ce256b023151eee98744d494d43c4
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2022-08-27 19:30:31 +0300

    add new records types for aws

--- aws.tf
+++ aws.tf
@@ -3,20 +3,47 @@ resource "aws_route53_zone" "this" {
   name  = var.name
 }
 
+resource "aws_route53_record" "a_records" {
+  for_each = contains(var.cloud_providers, "aws") ? try(local.records_aws["A"], {}) : {}
+  zone_id  = aws_route53_zone.this[0].zone_id
+  name     = each.value.name
+  type     = each.value.type
+  ttl      = each.value.ttl
+  records  = each.value.value
+}
+
+resource "aws_route53_record" "aaaa_records" {
+  for_each = contains(var.cloud_providers, "aws") ? try(local.records_aws["AAAA"], {}) : {}
+  zone_id  = aws_route53_zone.this[0].zone_id
+  name     = each.value.name
+  type     = each.value.type
+  ttl      = each.value.ttl
+  records  = each.value.value
+}
+
+resource "aws_route53_record" "cname_records" {
+  for_each = contains(var.cloud_providers, "aws") ? try(local.records_aws["CNAME"], {}) : {}
+  zone_id  = aws_route53_zone.this[0].zone_id
+  name     = each.value.name
+  type     = each.value.type
+  ttl      = each.value.ttl
+  records  = each.value.value
+}
+
 resource "aws_route53_record" "mx_records" {
-  for_each = flatten([
-    for name, records in local.records["MX"] : [
-      for record in records : {
-        name  = name,
-        type  = "MX",
-        ttl   = record.ttl,
-        value = "${record.priority} ${record.value}"
-      }
-    ]
-  ])
-  zone_id = aws_route53_zone.this[0].zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = each.value.ttl
-  records = each.value.records
+  for_each = contains(var.cloud_providers, "aws") ? try(local.records_aws["MX"], {}) : {}
+  zone_id  = aws_route53_zone.this[0].zone_id
+  name     = each.value.name
+  type     = each.value.type
+  ttl      = each.value.ttl
+  records  = each.value.value
+}
+
+resource "aws_route53_record" "txt_records" {
+  for_each = contains(var.cloud_providers, "aws") ? try(local.records_aws["TXT"], {}) : {}
+  zone_id  = aws_route53_zone.this[0].zone_id
+  name     = each.value.name
+  type     = each.value.type
+  ttl      = each.value.ttl
+  records  = each.value.value
 }
