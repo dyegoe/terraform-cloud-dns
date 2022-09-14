8dd238abd46ea9110403f329672e713f873b4c7a
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2022-09-14 20:04:58 +0300

    hotfix: change name_servers output

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "zone" {
     aws = !contains(var.cloud_providers, "aws") ? null : {
       name         = aws_route53_zone.this[0].name
       id           = aws_route53_zone.this[0].zone_id
-      name_servers = aws_route53_zone.this[0].name_servers
+      name_servers = [for ns in aws_route53_zone.this[0].name_servers : ns]
       dnssec = !var.dnssec ? {} : {
         key_tag = aws_route53_key_signing_key.this[0].key_tag
         digest  = aws_route53_key_signing_key.this[0].digest_value
