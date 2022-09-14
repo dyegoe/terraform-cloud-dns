e724cce7eba13303e65b5b933e04b9e981c3969c
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2022-09-14 20:11:38 +0300

    hotfix: if not dnssec = null

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "zone" {
       name         = aws_route53_zone.this[0].name
       id           = aws_route53_zone.this[0].zone_id
       name_servers = [for ns in aws_route53_zone.this[0].name_servers : ns]
-      dnssec = !var.dnssec ? "" : {
+      dnssec = !var.dnssec ? false : {
         key_tag = aws_route53_key_signing_key.this[0].key_tag
         digest  = aws_route53_key_signing_key.this[0].digest_value
       }
